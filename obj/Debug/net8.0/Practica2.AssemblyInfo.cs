01f75697059464415ae9cccc1916ba76d19eee0c
Dereck Estrada Molina <dereckdem17@gmail.com>
2024-07-14 12:56:03 -0500

    Pruebas git pull

--- obj/Debug/net8.0/Practica2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Practica2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4b0a9dc3e4a0461e53e5fa1399d493c58b4f98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+028445c41c87d6f43a4dba4c6fd1d9b289759a3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
